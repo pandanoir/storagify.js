0a2a67525766b69a35b55376bc09b42f0db7af85
pandanoir <moon_in_france@yahoo.co.jp>
2019-02-23 23:39:03 +0900

    refactor code

--- src/main.js
+++ src/main.js
@@ -1,41 +1,42 @@
+const isNestable = obj => Array.isArray(obj) || ({}).toString.call(obj) === ({}).toString.call({});
 const Storagify = (key, defaultVal) => {
-    const toString = ({}).toString;
-    const isNestable = obj => Array.isArray(obj) || toString.call(obj) === toString.call({});
-
     // ウォッチャーの設定
-    const handler = {
+    const watcher = {
         set(target, prop, value) {
-            if (isNestable(value)) Reflect.set(target, prop, new Proxy( nest(value), handler )); // valueがオブジェクトか配列だったらProxyを設定
-            else Reflect.set(target, prop, value);
+            if (isNestable(value))
+                Reflect.set(target, prop, new Proxy( deepWatcher(value), watcher )); // valueがオブジェクトか配列だったらProxyを設定
+            else
+                Reflect.set(target, prop, value);
 
-            localStorage.setItem(key, JSON.stringify(val)); // localStorageに変更後のオブジェクトを保存
+            localStorage.setItem(key, JSON.stringify(watchedValue)); // localStorageに変更後のオブジェクトを保存
             return true;
         }
     };
+    // ネストしているところにもプロキシを設定
+    const deepWatcher = (obj) => {
+        if (Array.isArray(obj))
+            return obj.map(item => {
+                if (!isNestable(item))
+                    return item;
+                return new Proxy(deepWatcher(item), watcher);
+            });
+
+        for (const key of Object.keys(obj)) {
+            if (!isNestable(obj[key]))
+                continue;
+            obj[key] = new Proxy(deepWatcher(obj[key]), watcher);
+        }
+        return obj;
+    };
 
     // localStorageに保存する処理
-    const keys = [...Array(localStorage.length)].map((_, n) => localStorage.key(n));;
-    const keyExists = keys.some(val => val === key); // キーが存在するか判定
-    const _val = keyExists ? JSON.parse(localStorage.getItem(key)) : defaultVal;
+    const localStorageKeys = [...Array(localStorage.length)].map((_, n) => localStorage.key(n));;
+    const keyExists = localStorageKeys.some(val => val === key);
+    const value = (keyExists ? JSON.parse(localStorage.getItem(key)) : defaultVal);
 
-    // ネストしているところにもプロキシを設定
-    const nest = (obj) => {
-        if (Array.isArray(obj))
-            for (let i = 0, _i = obj.length; i < _i; i++) {
-                if (isNestable(obj[i]))
-                    obj[i] = new Proxy(nest(obj[i]), handler);
-            }
-        else
-            for (const key of Object.keys(obj)) {
-                if (isNestable(obj[key]))
-                    obj[key] = new Proxy(nest(obj[key]), handler);
-            }
-        return obj;
-    }
+    const watchedValue = new Proxy( deepWatcher(value), watcher );
+    localStorage.setItem(key, JSON.stringify(watchedValue));
 
-    const val = new Proxy( nest(_val), handler );
-    localStorage.setItem(key, JSON.stringify(val));
-
-    return val;
+    return watchedValue;
 };
 export default Storagify;
